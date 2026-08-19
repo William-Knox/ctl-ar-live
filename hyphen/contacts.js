2df56ca4665ec248d388ec42ef02a808e659d0d9
William-Knox <william@coastaltestinglabs.com>
2026-08-19 08:48:34 +0000

    Hyphen 7:30 2026-08-19: start-to-finish rework, Issue tree, 481 rows

---
+++ hyphen/contacts.js
@@ -0,0 +1,149 @@
+window.CONTACTS = {
+  "updated": "2026-08-18",
+  "rule": "To/CC come from this list by builder + community. Never the dirt contractor. Never HOU.acctspayable. Hunter Hott is never on a Tri Pointe chase. Update when Maria/billings mail shows a new person or a bounce.",
+  "never": {
+    "Tri Pointe": [
+      "hunter@hotttrucking.com",
+      "veronica@hotttrucking.com",
+      "HOU.acctspayable@tripointehomes.com",
+      "hou.acctspayable@tripointehomes.com"
+    ]
+  },
+  "builders": {
+    "Tri Pointe": {
+      "aliases": [
+        "TriPointe Homes",
+        "Tri Pointe Homes",
+        "Trendmaker"
+      ],
+      "communities": {
+        "Woodson's Reserve": {
+          "aliases": [
+            "Woodsons Reserve",
+            "Woodson Reserve",
+            "Woodson's"
+          ],
+          "to": [
+            {
+              "name": "Chris Tschumperlin",
+              "email": "chris.tschumperlin@tripointehomes.com"
+            }
+          ],
+          "cc": [
+            {
+              "name": "Derrick Chin",
+              "email": "Derrick.Chin@TriPointeHomes.com"
+            },
+            {
+              "name": "Bart Mills",
+              "email": "bart.mills@tripointehomes.com"
+            },
+            {
+              "name": "Dusty Westbrook",
+              "email": "Dusty.Westbrook@TriPointeHomes.com"
+            },
+            {
+              "name": "Jake Matcheski",
+              "email": "jake.matcheski@tripointehomes.com"
+            },
+            {
+              "name": "Cindy Lopez",
+              "email": "cindy.lopez@tripointehomes.com"
+            },
+            {
+              "name": "Craig Borski",
+              "email": "craig.borski@tripointehomes.com"
+            }
+          ],
+          "report_only": [
+            {
+              "name": "Compaction tests",
+              "email": "compactiontests@trendmakerhomes.com"
+            },
+            {
+              "name": "Darron Thomas",
+              "email": "darron.thomas@tripointehomes.com"
+            }
+          ],
+          "source": "Maria report 2026-04-17 and PO Request 2026-06-26 for 0416-170658 / 28502 Pebble Springs. Contractor Hunter Hott called the work in; he is not a recipient.",
+          "updated": "2026-08-18"
+        },
+        "Legacy Grove": {
+          "aliases": [
+            "21-506 Legacy Grove",
+            "Heritage Grove"
+          ],
+          "to": [
+            {
+              "name": "Jake Matcheski",
+              "email": "jake.matcheski@tripointehomes.com"
+            }
+          ],
+          "cc": [
+            {
+              "name": "Chris Tschumperlin",
+              "email": "chris.tschumperlin@tripointehomes.com"
+            }
+          ],
+          "source": "Maria report 2026-04-30 (To Jake) and Proctor PO Request 2026-06-26 (To Jake and Chris) for 0428-170954 / 3003 Heritage Grove Ct. Jake called the work in and is the paying CM on Maria mail. Not copied from Woodson's Reserve.",
+          "updated": "2026-08-18"
+        },
+        "Pecan Ridge": {
+          "aliases": [
+            "Sky Branch"
+          ],
+          "to": [
+            {
+              "name": "Chad Luedke",
+              "email": "chad.luedke@tripointehomes.com"
+            }
+          ],
+          "cc": [
+            {
+              "name": "Josh Runnells",
+              "email": "josh.runnells@tripointehomes.com"
+            },
+            {
+              "name": "Keith Barrett",
+              "email": "keith.barrett@tripointehomes.com"
+            },
+            {
+              "name": "Jerry Battle",
+              "email": "jerry.battle@tripointehomes.com"
+            }
+          ],
+          "source": "Maria/billings Pecan Ridge invoice and no-PO threads (Sky Branch, Leaf Drift, Oakside).",
+          "updated": "2026-08-18"
+        }
+      }
+    }    ,
+    "Lennar": {
+      "aliases": [
+        "LENNAR HOMES",
+        "Lennar Homes"
+      ],
+      "communities": {
+        "Grand Lake": {
+          "aliases": [
+            "98-639 Grand Lake",
+            "Wharton Lakes"
+          ],
+          "to": [
+            {
+              "name": "Rodney Shewell",
+              "email": "rodney.shewell@lennar.com"
+            }
+          ],
+          "cc": [
+            {
+              "name": "John Helms",
+              "email": "john.helms@lennar.com"
+            }
+          ],
+          "source": "Jocelyne report 2026-05-07 To Rodney Shewell for 0506-171310 / 1220 Charlotte Way. John Helms called the work in 5/1 and was on Rodney's 5/6 CC.",
+          "updated": "2026-08-18"
+        }
+      }
+    }
+  }
+};
